99733cbaada4b2d657fcbe016e53a6a4491830dd
vidhya-metacell <vidhya@metacell.us>
2022-08-02 16:36:25 +0200

    #31 Import default link widget form react-diagrams, change props to match that

--- src/components/views/projections/CustomLinkWidget.js
+++ src/components/views/projections/CustomLinkWidget.js
@@ -1,25 +1,26 @@
 import * as React from "react";
 import {
     DefaultLinkWidget, LinkWidget
-} from 'meta-diagram';
+} from '@projectstorm/react-diagrams';
 
 const CustomLinkArrowWidget = (props) => {
     const {point, arrowHeadXPosition} = props;
 		const pointlength = 7;
     return (
-			// TODO: Replace with variant for style options, when latest meta is merged
+		// TODO: Replace with variant for style options, when latest meta is merged
         <g className="arrow" transform={'translate(' + (point.getPosition().x - arrowHeadXPosition) + ', ' + (point.getPosition().y - pointlength) + ')'}>
-						<g>
-								<polyline
-										points={`0,0 ${pointlength},${pointlength} 0,${pointlength*2}`}
-										stroke="#3C3C43"
-										strokeWidth="2"
-										strokeOpacity="0.6"
-										fill="none"
-										data-id={point.getID()}
-										data-linkid={point.getLink().getID()}
-								/>
-						</g>
+            <g>
+                <polyline
+                    points={`0,0 ${pointlength},${pointlength} 0,${pointlength*2}`}
+                    stroke="#3C3C43"
+                    strokeWidth="2"
+                    strokeOpacity="0.6"
+                    strokeLinecap="round"
+                    fill="none"
+                    data-id={point.getID()}
+                    data-linkid={point.getLink().getID()}
+                />
+            </g>
         </g>
     );
 };
@@ -45,16 +46,17 @@ class CustomLink extends React.Component {
     }
 
     render() {
-			// TODO: Replace with variant for style options, when latest meta is merged
+		// TODO: Replace with variant for style options, when latest meta is merged
         return (
             <g>
                 <path
                     ref={this.path}
                     stroke="#3C3C43"
-										strokeOpacity="0.6"
-										strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeOpacity="0.6"
+                    strokeWidth="2"
                     d={this.props.path}
-								/>
+				/>
             </g>
         );
     }
@@ -68,7 +70,7 @@ export class CustomLinkWidget extends DefaultLinkWidget {
             <CustomLinkArrowWidget
                 key={`link-arrow-${point.getID()}`}
                 point={point}
-								arrowHeadXPosition={arrowHeadXPosition}
+				arrowHeadXPosition={arrowHeadXPosition}
                 colorSelected={this.props.link.getOptions().selectedColor}
                 color={this.props.link.getOptions().color}
             />
@@ -98,8 +100,8 @@ export class CustomLinkWidget extends DefaultLinkWidget {
         }
 
         if (this.props.link.getTargetPort() !== null) {
-						const arrowHeadXPosition = (points[points.length - 1].getPosition().x - points[0].getPosition().x)/6;
-						paths.push(this.generateArrow(points[points.length - 1], points[points.length - 2], arrowHeadXPosition));
+                const arrowHeadXPosition = (points[points.length - 1].getPosition().x - points[0].getPosition().x)/6;
+                paths.push(this.generateArrow(points[points.length - 1], points[points.length - 2], arrowHeadXPosition));
         } else {
             paths.push(this.generatePoint(points[points.length - 1]));
         }
@@ -117,9 +119,9 @@ export class CustomLinkWidget extends DefaultLinkWidget {
 
 class CustomLinkAdapter extends React.Component {
     render() {
-        const {link, diagramEngine} = this.props;
+        const {model, engine} = this.props;
         return (
-            <CustomLinkWidget link={link} diagramEngine={diagramEngine}/>
+            <CustomLinkWidget link={model} diagramEngine={engine}/>
         );
     }
 }
