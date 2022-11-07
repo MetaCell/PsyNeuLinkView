4388e421f4d0a0e9775e03c83e9744f130094f6b
emekauja <ujadninth@gmail.com>
2022-11-07 22:59:21 +0100

    #45 added new icons

--- src/components/views/Sidebar/TreeView/Icons.js
+++ src/components/views/Sidebar/TreeView/Icons.js
@@ -20,7 +20,7 @@ export const FileIcon = (props) => (
 export const ShapeArrowToolIcon = (props) => (
   <svg
     width={11}
-    height={11}
+    height={10}
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
     {...props}
@@ -32,64 +32,68 @@ export const ShapeArrowToolIcon = (props) => (
   </svg>
 );
 
-export const CircleIcon = (props) => (
-  <svg
-    width={14}
-    height={11}
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-    {...props}
-  >
-    <g filter="url(#a)" shapeRendering="crispEdges">
-      <rect
-        x={2}
-        y={1}
-        width={10}
-        height={10}
-        rx={5}
-        fill="#8F8F8F"
-        fillOpacity={0.2}
-      />
-      <rect
-        x={2.5}
-        y={1.5}
-        width={9}
-        height={9}
-        rx={4.5}
-        stroke={props.color ?? '#8F8F8F'}
-      />
-    </g>
-    <defs>
-      <filter
-        id="a"
-        x={0}
-        y={0}
-        width={14}
-        height={14}
-        filterUnits="userSpaceOnUse"
-        colorInterpolationFilters="sRGB"
-      >
-        <feFlood floodOpacity={0} result="BackgroundImageFix" />
-        <feColorMatrix
-          in="SourceAlpha"
-          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
-          result="hardAlpha"
-        />
-        <feOffset dy={1} />
-        <feGaussianBlur stdDeviation={1} />
-        <feComposite in2="hardAlpha" operator="out" />
-        <feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.1 0" />
-        <feBlend in2="BackgroundImageFix" result="effect1_dropShadow_818_5" />
-        <feBlend
-          in="SourceGraphic"
-          in2="effect1_dropShadow_818_5"
-          result="shape"
-        />
-      </filter>
-    </defs>
-  </svg>
-);
-
+export function CircleIcon(props) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="14"
+      height="12"
+      fill="none"
+      viewBox="0 0 14 12"
+    >
+      <g filter="url(#filter0_d_488_8120)" shapeRendering="crispEdges">
+        <rect
+          width="10"
+          height="10"
+          x="2"
+          y="1"
+          fill={props.color ?? '#8F8F8F'}
+          fillOpacity="0.2"
+          rx="5"
+        ></rect>
+        <rect
+          width="9"
+          height="9"
+          x="2.5"
+          y="1.5"
+          stroke="#8F8F8F"
+          rx="4.5"
+        ></rect>
+      </g>
+      <defs>
+        <filter
+          id="filter0_d_488_8120"
+          width="14"
+          height="14"
+          x="0"
+          y="0"
+          colorInterpolationFilters="sRGB"
+          filterUnits="userSpaceOnUse"
+        >
+          <feFlood floodOpacity="0" result="BackgroundImageFix"></feFlood>
+          <feColorMatrix
+            in="SourceAlpha"
+            result="hardAlpha"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+          ></feColorMatrix>
+          <feOffset dy="1"></feOffset>
+          <feGaussianBlur stdDeviation="1"></feGaussianBlur>
+          <feComposite in2="hardAlpha" operator="out"></feComposite>
+          <feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.1 0"></feColorMatrix>
+          <feBlend
+            in2="BackgroundImageFix"
+            result="effect1_dropShadow_488_8120"
+          ></feBlend>
+          <feBlend
+            in="SourceGraphic"
+            in2="effect1_dropShadow_488_8120"
+            result="shape"
+          ></feBlend>
+        </filter>
+      </defs>
+    </svg>
+  );
+}
 export const CloseIcon = (props) => (
   <svg
     width={12}
@@ -138,6 +142,38 @@ export const HiddenIcon = (props) => (
       clipRule="evenodd"
       d="M13.508 1.801A8.031 8.031 0 0 0 14.93 0h-1.185A6.992 6.992 0 0 1 8 3a6.992 6.992 0 0 1-5.746-3H1.07a8.032 8.032 0 0 0 1.421 1.801L.896 3.396l.708.707L3.26 2.446c.71.523 1.511.932 2.374 1.199l-.617 2.22.964.269.626-2.255a8.05 8.05 0 0 0 2.784 0l.626 2.255.964-.268-.617-2.221a7.974 7.974 0 0 0 2.374-1.2l1.657 1.658.708-.707-1.595-1.595Z"
       fill="#1A1A1A"
+      fillOpacity={0.3}
+    />
+  </svg>
+);
+
+export const ArrowDropDownIcon = (props) => (
+  <svg
+    width={8}
+    height={8}
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      d="M.147 2.853 3.183 5.89a.5.5 0 0 0 .707 0l3.035-3.036A.5.5 0 0 0 6.572 2H.5a.5.5 0 0 0-.354.853Z"
+      fill="#000"
+      fillOpacity={0.3}
+    />
+  </svg>
+);
+
+export const ArrowRightIcon = (props) => (
+  <svg
+    width={8}
+    height={8}
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      d="m2.372 7.389 3.036-3.036a.5.5 0 0 0 0-.707L2.372.611a.5.5 0 0 0-.853.353v6.071a.5.5 0 0 0 .853.354Z"
+      fill="#000"
       fillOpacity={0.3}
     />
   </svg>
