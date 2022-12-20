302f07d73c35ada58d13893c7b89f10c9c02ed16
emekauja <ujadninth@gmail.com>
2022-12-20 19:40:09 +0100

    reconcile feature/64, 66, 67

--- src/components/views/visualiseView/MainVisualise.js
+++ src/components/views/visualiseView/MainVisualise.js
@@ -1,10 +1,21 @@
+import { makeStyles } from '@mui/styles';
+import { Box } from '@mui/system';
 import React from 'react';
 
+const useStyles = makeStyles(() => ({
+  root: {
+    display: 'flex',
+    alignItems: 'center',
+  }
+}))
+
+// add left sidebar and layout manager
 const MainVisualise = ({ children }) => {
+  const classes = useStyles()
   return (
-    <div>
+    <Box className={classes.root}>
         This is the view where to instantiate the layout manager
-    </div>
+    </Box>
   );
 };
 
