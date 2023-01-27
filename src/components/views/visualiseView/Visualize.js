0edbe515716ce54f660a658209d90286631cd423
emekauja <ujadninth@gmail.com>
2023-01-27 13:09:10 +0100

    fix: remove redundant code

--- src/components/views/visualiseView/Visualize.js
+++ src/components/views/visualiseView/Visualize.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { makeStyles } from '@mui/styles';
 import MainLayout from '../../../layout/visualise/main';
 import Sidebar from './sidebar/sidebar';
 import { properties } from './sidebar/mock/properties';
@@ -7,15 +6,7 @@ import VisualizeLayout from './VisualizeLayout';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 
-const useStyles = makeStyles(() => ({
-  root: {
-    display: 'flex',
-    alignItems: 'center',
-  },
-}));
-
-const Visualize = ({ children }) => {
-  const classes = useStyles();
+const Visualize = () => {
   return (
     <DndProvider backend={HTML5Backend}>
       <MainLayout sidebar={<Sidebar properties={properties} />}>
