8389def51f7629a053a386ed75ac0bca802f67d6
emekauja <ujadninth@gmail.com>
2023-02-25 23:48:27 +0100

    PSYNEU-19: update line chart with custom legend

--- src/components/views/visualiseView/main/charts/lineChart.js
+++ src/components/views/visualiseView/main/charts/lineChart.js
@@ -1,116 +1,13 @@
 import React, { useState } from 'react';
 import Plot from 'react-plotly.js';
-import { alpha, Box } from '@mui/system';
 import RANDOM_COLORS from '../../../../../assets/styles/random-colors';
-import { horizontalLayout, modebar } from './chartLayout';
-import { Paper, Stack, Typography } from '@mui/material';
-import { makeStyles } from '@mui/styles';
-import vars from '../../../../../assets/styles/variables';
+import { horizontalLayout } from './chartLayout';
+import { Paper, Typography } from '@mui/material';
 import { mock } from './mock';
-
-const { breadcrumbLinkColor } = vars;
-
-const OPACITY = 0.4;
-
-const getRandomColors = (data) => {
-  if (data) {
-    return data.length > 0
-      ? data.map((item, index) => RANDOM_COLORS[index % 200])
-      : [];
-  }
-};
-
-function getInitialChartData(data) {
-  let chartData;
-
-  const randomColors = getRandomColors(data);
-  if (data !== undefined) {
-    chartData = data.reduce((prev, current, currentIndex) => {
-      let chartItem;
-      const color = randomColors[currentIndex];
-
-      if (current !== undefined) {
-        const x = current.info?.map((d) => d.x);
-        const text = current.info?.map((d) => d.text);
-        const y = current.info?.map((d) => d.y);
-        chartItem = {
-          x,
-          y,
-          text,
-          name: current.name,
-          type: 'scatter',
-          mode: 'lines',
-          line: {
-            color,
-          },
-        };
-
-        prev.push(chartItem);
-      }
-      return prev;
-    }, []);
-
-    return chartData;
-  }
-}
-
-function getInitialLegendData(data) {
-  let legendData;
-
-  const randomColors = getRandomColors(data);
-  if (data !== undefined) {
-    legendData = data.reduce((prev, current, currentIndex) => {
-      let legendItem;
-      const color = randomColors[currentIndex];
-      if (current !== undefined) {
-        legendItem = {
-          name: current.name,
-          color,
-          visible: true,
-        };
-
-        prev.push(legendItem);
-      }
-      return prev;
-    }, []);
-
-    return legendData;
-  }
-}
-
-const useStyles = makeStyles(() => ({
-  topRight: {
-    position: 'absolute',
-    top: 18,
-    left: 16,
-    overflowX: 'hidden',
-    width: '60%',
-    maxWidth: '60%',
-  },
-  legend: {
-    overflowX: 'scroll',
-    scrollbarWidth: 'none' /* Firefox */,
-
-    '&::-webkit-scrollbar': {
-      display: 'none',
-    },
-
-    '& .item': {
-      cursor: 'pointer',
-      flexShrink: 0,
-      '& .MuiTypography-root': {
-        fontSize: '0.625rem',
-        fontWeight: 500,
-        color: breadcrumbLinkColor,
-        lineHeight: '1rem',
-        // wordWrap: 'normal',
-      },
-    },
-  },
-}));
+import CustomLegend from './CustomLegend';
+import { getInitialChartData, getInitialLegendData } from './util';
 
 const LineChart = ({ initialData = mock }) => {
-  const classes = useStyles();
   const [data, setData] = useState(() => getInitialChartData(mock));
 
   const [hover, setHover] = useState({
@@ -179,33 +76,10 @@ const LineChart = ({ initialData = mock }) => {
             <Typography>y: {hover.y}</Typography>
           </Paper>
         )}
-        <Box className={classes.topRight}>
-          <Stack direction="row" spacing={1} className={classes.legend}>
-            {legend.map((item, index) => {
-              console.log(item, 'item');
-              return (
-                <Stack
-                  key={index}
-                  direction="row"
-                  alignItems="center"
-                  onClick={() => updateVisibility(index)}
-                  spacing={0.5}
-                  className="item"
-                >
-                  <div
-                    style={{
-                      height: 10,
-                      width: 10,
-                      backgroundColor: item.color,
-                      borderRadius: '2px',
-                    }}
-                  />
-                  <Typography>{item?.name}</Typography>
-                </Stack>
-              );
-            })}
-          </Stack>
-        </Box>
+        <CustomLegend
+          legend={legend}
+          onClick={(index) => updateVisibility(index)}
+        />
       </>
     </div>
   );
