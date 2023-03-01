618bf2d75894aec271115f89f5a7c850388a5816
emekauja <ujadninth@gmail.com>
2023-03-01 12:02:59 +0100

    fix: added new helpers, change initial chart logic

--- src/components/views/visualiseView/main/charts/util.js
+++ src/components/views/visualiseView/main/charts/util.js
@@ -26,18 +26,19 @@ export function getInitialChartData(data, chartInfo) {
       const color = randomColors[currentIndex];
 
       if (current !== undefined) {
-        const x = current.info?.map((d) => d.x);
-        const text = current.info?.map((d) => d.text);
-        const y = current.info?.map((d) => d.y);
         chartItem = {
-          x,
-          y,
-          text,
+          x: current.x,
+          y: current.y,
+          text: current.text,
           name: current.name,
           type: chartInfo?.type ?? 'scatter',
           mode: chartInfo?.mode ?? 'lines',
           line: {
             color,
+          },
+          marker: {
+            color,
+            size: 12,
           },
         };
 
@@ -79,4 +80,35 @@ export function getInitialLegendData(data) {
 
     return legendData;
   }
+}
+
+/**
+ * Default: return a array of number base on length
+ *
+ * @param {Integer} length Desired length
+ * @param {Integer} max Desired maximum number
+ * @return {Array}
+ */
+export const randomArray = (length, max) =>
+  Array(length)
+    .fill()
+    .map(() => Math.round(Math.random() * max));
+
+/**
+ * Pseudo-random string generator
+ * http://stackoverflow.com/a/27872144/383904
+ * Default: return a random alpha-numeric string
+ *
+ * @param {Integer} length Desired length
+ * @return {String}
+ */
+export function randomString(length) {
+  var str = ''; // String result
+  for (var i = 0; i < length; i++) {
+    // Loop `length` times
+    var rand = Math.floor(Math.random() * 62); // random: 0..61
+    var charCode = (rand += rand > 9 ? (rand < 36 ? 55 : 61) : 48); // Get correct charCode
+    str += String.fromCharCode(charCode); // add Character to str
+  }
+  return str; // After all loops are done, return the concatenated string
 }
