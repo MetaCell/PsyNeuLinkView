1d93515d18dfae3a2f75263f472d5ece6c79f513
afonso <afonsobspinto@gmail.com>
2023-03-21 15:55:56 +0000

    PSYNEU-30 chore: Add comments to clippingService

--- src/services/clippingService.ts
+++ src/services/clippingService.ts
@@ -2,6 +2,12 @@ import {MetaNodeModel} from "@metacell/meta-diagram";
 import {PointModel, PortModel} from "@projectstorm/react-diagrams-core";
 import {Point} from "@projectstorm/geometry";
 
+/**
+ * Calculates the amount by which the child element extends beyond the parent element in each direction (left, right, top, bottom).
+ * @param parentElement The parent element.
+ * @param childElement The child element.
+ * @returns An object containing the bounding boxes of the parent and child elements, and the amount by which the child extends beyond the parent in each direction.
+ */
 export function getOutsideData(parentElement: Element, childElement: Element) {
     if(!parentElement || !childElement){
         return null
@@ -18,6 +24,12 @@ export function getOutsideData(parentElement: Element, childElement: Element) {
     return {childBoundingBox, parentBoundingBox, outsideData};
 }
 
+/**
+ * Calculates the amount by which a border of the child element should be offset based on how much the child extends beyond the parent in each direction.
+ * @param outsideData An object containing the amount by which the child extends beyond the parent in each direction.
+ * @param borderSize The size of the border.
+ * @returns An object containing the amount by which each border should be offset.
+ */
 function getBorderOffset(outsideData: { top: number; left: number; bottom: number; right: number }, borderSize: number) {
     const rightBorderOffset = outsideData.right > 0 ? borderSize : 0
     const leftBorderOffset = outsideData.left > 0 ? borderSize : 0
@@ -26,17 +38,58 @@ function getBorderOffset(outsideData: { top: number; left: number; bottom: numbe
     return {rightBorderOffset, leftBorderOffset, topBorderOffset, bottomBorderOffset};
 }
 
+/**
+
+ Returns a string representation of a polygon with the specified coordinates.
+ @param left - The x-coordinate of the left vertex.
+ @param top - The y-coordinate of the top vertex.
+ @param right - The x-coordinate of the right vertex.
+ @param bottom - The y-coordinate of the bottom vertex.
+ @returns A string representation of a polygon with the specified coordinates.
+ */
+
 function getClipPathStr(left: number, top: number, right: number, bottom: number) {
     return `polygon(${left}px ${top}px, ${right}px ${top}px,${right}px ${bottom}px, ${left}px ${bottom}px)`;
 }
+
+/**
+
+ Calculates the distance between the child element's bottom edge and the bottom edge of the parent element, taking into account the size of the parent element's border and a scale factor.
+ @param childBoundingBox - The child element's bounding box.
+ @param outsideData - An object with the amount by which the child element overflows each side of the parent element.
+ @param bottomBorderOffset - The offset of the parent element's bottom border.
+ @param scaleFactor - A scale factor to apply to the distance calculation.
+ @returns The distance between the child element's bottom edge and the bottom edge of the parent element.
+ */
 
 function getBottom(childBoundingBox: DOMRect, outsideData: { top: number; left: number; bottom: number; right: number }, bottomBorderOffset: number, scaleFactor: number) {
     return (childBoundingBox.height - outsideData.bottom - bottomBorderOffset) / scaleFactor;
 }
 
+/**
+
+ Calculates the distance between the child element's right edge and the right edge of the parent element, taking into account the size of the parent element's border and a scale factor.
+ @param childBoundingBox - The child element's bounding box.
+ @param outsideData - An object with the amount by which the child element overflows each side of the parent element.
+ @param rightBorderOffset - The offset of the parent element's right border.
+ @param scaleFactor - A scale factor to apply to the distance calculation.
+ @returns The distance between the child element's right edge and the right edge of the parent element.
+ */
+
+
 function getRight(childBoundingBox: DOMRect, outsideData: { top: number; left: number; bottom: number; right: number }, rightBorderOffset: number, scaleFactor: number) {
     return (childBoundingBox.width - outsideData.right - rightBorderOffset) / scaleFactor;
 }
+
+/**
+
+ Calculates the x and y coordinates of the top-left corner of the clip path, taking into account the size of the parent element's border and a scale factor.
+ @param outsideData - An object with the amount by which the child element overflows each side of the parent element.
+ @param leftBorderOffset - The offset of the parent element's left border.
+ @param scaleFactor - A scale factor to apply to the distance calculation.
+ @param topBorderOffset - The offset of the parent element's top border.
+ @returns An object containing the x and y coordinates of the top-left corner of the clip path.
+ */
 
 function getLeftTop(outsideData: { top: number; left: number; bottom: number; right: number }, leftBorderOffset: number, scaleFactor: number, topBorderOffset: number) {
     const left = (outsideData.left + leftBorderOffset) / scaleFactor;
@@ -44,6 +97,16 @@ function getLeftTop(outsideData: { top: number; left: number; bottom: number; ri
     return {left, top};
 }
 
+/**
+ * Returns a clip path as a string for a child element to be clipped by its parent element.
+ *
+ * @param {Element | null} parentElement - The parent element to clip the child element.
+ * @param {Element | null} childElement - The child element to be clipped by its parent element.
+ * @param {number} borderSize - The size of the border of the child element.
+ * @param {number} scaleFactor - The scaling factor for the clip path.
+ *
+ * @returns {string | null} A string representing the polygon clip path or null if either parent or child element is null.
+ */
 export function getClipPath(parentElement: Element | null, childElement: Element | null, borderSize: number, scaleFactor: number) {
     if(!parentElement || !childElement){
         return null;
@@ -70,10 +133,24 @@ export function isAnyDirectionOutside(outsideData: { top: number; left: number; 
     return outsideData.top > 0 || outsideData.bottom > 0 || outsideData.left > 0 || outsideData.right > 0;
 }
 
+/**
+ * Returns the ID of the parent node for a given node in the meta diagram.
+ * If the node is at the top level, returns itself
+ * @param node The node for which to get the parent ID.
+ * @returns The ID of the parent node.
+ */
+
 export function getParentNodeId(node: MetaNodeModel) {
     return node.getGraphPath().length === 1 ? node.getGraphPath()[0] : node.getGraphPath()[node.getGraphPath().length - 2]
 }
 
+/**
+ * Returns a PointModel representing the nearest point on the parent to a given link's port
+ * @param parent The parent node to find the nearest border point on.
+ * @param originalPort The original source or target port of the link.
+ * @param link The link
+ * @returns A PointModel representing the nearest port on the parent node's border.
+ */
 export function getNearestParentPointModel(parent: MetaNodeModel, originalPort: PortModel, link: any){
     let yPos = originalPort.getY()
     let xPos = originalPort.getX()
