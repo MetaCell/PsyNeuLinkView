bda5059f6b611f9aa728a0096b65de163b52bb33
emekauja <ujadninth@gmail.com>
2023-04-14 18:03:28 +0100

    psynue-31: fix: behavior on move

--- src/components/views/editView/leftSidebar/dropCallback.js
+++ src/components/views/editView/leftSidebar/dropCallback.js
@@ -1,11 +1,21 @@
-import { MetaNodeModel, MetaPort, PortTypes } from '@metacell/meta-diagram';
+import {
+  MetaNode,
+  MetaNodeModel,
+  MetaPort,
+  PortTypes,
+} from '@metacell/meta-diagram';
 import { PNLClasses } from '../../../../constants';
 import { updateMechanismCount } from '../../../../redux/actions/general';
 import pnlStore from '../../../../redux/store';
+import MechanismNode from '../../../../model/nodes/mechanism/MechanismNode';
+import { Point } from '@projectstorm/geometry';
 
 export function onNodeDrop(monitor, node, engine) {
   const options = new Map();
-  const position = monitor?.getClientOffset();
+  const offset = monitor?.getClientOffset();
+  const height = 619;
+  const width = 266;
+  const position = { x: offset.x - width / 4, y: offset.y - height / 4 };
   pnlStore.dispatch(updateMechanismCount());
   const currentCount = pnlStore.getState().general.mechanismCount;
   const name = `${node.name} ${currentCount}`;
@@ -13,18 +23,19 @@ export function onNodeDrop(monitor, node, engine) {
   options.set('id', name);
   options.set('name', name);
   options.set('variant', 'node-blue');
-  options.set('width', 266);
-  options.set('height', 619);
+  options.set('width', width);
+  options.set('height', height);
   options.set('selected', false);
   options.set('pnlClass', PNLClasses.MECHANISM);
   options.set('shape', PNLClasses.MECHANISM);
-  options.set('graphPath', ['Composition-0', 'input']);
+  options.set('graphPath', [null]);
+  options.set('depth', 0);
   options.set('ports', [
     new MetaPort('in', 'in', PortTypes.INPUT_PORT, undefined, undefined),
     new MetaPort('out', 'out', PortTypes.OUTPUT_PORT, undefined, undefined),
   ]);
-  options.set('position', position);
-  options.set('localPosition', position);
+  options.set('position', new Point(position.x, position.y));
+  options.set('localPosition', new Point(position.x, position.y));
 
   const newNode = new MetaNodeModel(Object.fromEntries(options));
 
