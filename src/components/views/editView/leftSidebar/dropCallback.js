7344b15c6c4f0f0b024e3bc65c0df9b0a7056a58
emekauja <ujadninth@gmail.com>
2023-05-09 17:28:23 +0100

    PSYNEU36:chore: Composition&Projection instantiate

--- src/components/views/editView/leftSidebar/dropCallback.js
+++ src/components/views/editView/leftSidebar/dropCallback.js
@@ -1,5 +1,5 @@
 import {
-  MetaNode,
+  MetaLinkModel,
   MetaNodeModel,
   MetaPort,
   PortTypes,
@@ -7,7 +7,6 @@ import {
 import { PNLClasses } from '../../../../constants';
 import { updateMechanismCount } from '../../../../redux/actions/general';
 import pnlStore from '../../../../redux/store';
-import MechanismNode from '../../../../model/nodes/mechanism/MechanismNode';
 import { Point } from '@projectstorm/geometry';
 
 export function onNodeDrop(monitor, node, engine) {
@@ -19,27 +18,56 @@ export function onNodeDrop(monitor, node, engine) {
   pnlStore.dispatch(updateMechanismCount());
   const currentCount = pnlStore.getState().general.mechanismCount;
   const name = `${node.name} ${currentCount}`;
-
-  options.set('id', name);
-  options.set('name', name);
-  options.set('variant', 'node-blue');
-  options.set('width', width);
-  options.set('height', height);
-  options.set('selected', false);
-  options.set('pnlClass', PNLClasses.MECHANISM);
-  options.set('shape', PNLClasses.MECHANISM);
-  options.set('graphPath', [null]);
-  options.set('depth', 0);
-  options.set('ports', [
-    new MetaPort('in', 'in', PortTypes.INPUT_PORT, undefined, undefined),
-    new MetaPort('out', 'out', PortTypes.OUTPUT_PORT, undefined, undefined),
-  ]);
-  options.set('position', new Point(position.x, position.y));
-  options.set('localPosition', new Point(position.x, position.y));
-
-  const newNode = new MetaNodeModel(Object.fromEntries(options));
+  const nodeType = node.type.toUpperCase();
 
   if (engine) {
-    return engine.getModel().addNode(newNode);
+    const model = engine.getModel();
+    const selectedNodes = engine.getModel().getSelectedEntities();
+
+    if (node.type === PNLClasses.PROJECTION) {
+      if (selectedNodes.length !== 2) return;
+
+      options.set('id', name);
+      options.set('name', name);
+      options.set('variant', PNLClasses[nodeType]);
+      options.set('shape', PNLClasses[nodeType]);
+      options.set('color', PNLClasses[nodeType]);
+      const sourcePort = selectedNodes[0].getPort(
+        selectedNodes[0].getOptions().ports[1].getName()
+      );
+      const targetPort = selectedNodes[1].getPort(
+        selectedNodes[1].getOptions().ports[0].getName()
+      );
+
+      const link = new MetaLinkModel(Object.fromEntries(options));
+      link.setSourcePort(sourcePort);
+      link.setTargetPort(targetPort);
+
+      model.addLink(link);
+      engine.repaintCanvas();
+    } else {
+      options.set('id', name);
+      options.set('name', name);
+      options.set('variant', 'node-blue');
+      options.set('width', width);
+      options.set('height', height);
+      options.set('selected', false);
+      options.set('pnlClass', PNLClasses[nodeType]);
+      options.set('shape', PNLClasses[nodeType]);
+      options.set('graphPath', [null]);
+      options.set('depth', 0);
+      options.set('ports', [
+        new MetaPort('in', 'in', PortTypes.INPUT_PORT, undefined, undefined),
+        new MetaPort('out', 'out', PortTypes.OUTPUT_PORT, undefined, undefined),
+      ]);
+      options.set('position', new Point(position.x, position.y));
+      options.set('localPosition', new Point(position.x, position.y));
+
+      const newNode = new MetaNodeModel(Object.fromEntries(options));
+      return model.addNode(newNode);
+    }
+
+    // const state = engine.getStateMachine().getCurrentState();
+    // state.dragCanvas.config.allowDrag = !state.dragCanvas.config.allowDrag;
   }
 }
