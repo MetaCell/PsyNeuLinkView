6f471b94d2378c228a3d3a5cf742e838d52cc451
ddelpiano <dario@metacell.us>
2023-06-22 12:02:17 +0100

    psyneu-73 composition resizing

--- src/client/components/views/editView/compositions/Composition.js
+++ src/client/components/views/editView/compositions/Composition.js
@@ -92,6 +92,10 @@ class Composition extends React.Component {
       subGraphOpen: false,
       width: props.model.options.width,
       height: props.model.options.height,
+      x: 0,
+      y: 0,
+      xUpdated: false,
+      yUpdated: false,
     }
     this.changeVisibility = this.changeVisibility.bind(this);
   }
@@ -104,8 +108,6 @@ class Composition extends React.Component {
     const { expanded } = this.state;
     const { classes } = this.props;
 
-
-
     return (
         <Box
           style={{width: this.state.width, height: this.state.height}}
@@ -113,22 +115,86 @@ class Composition extends React.Component {
         >
           <Rnd
             size={{ width: this.state.width, height: this.state.height }}
-            position={{ x: this.props.model.options
-                  .x, y: this.props.model.options.y }}
+            position={{ x: this.state.x, y: this.state.y }}
+            onResize={(e, direction, ref, delta, position) => {
+              switch (direction) {
+                case 'top':
+                  this.setState({
+                    y: e.clientY,
+                    yUpdated: true,
+                    height: ref.style.height,
+                  });
+                  break;
+                case 'right':
+                  this.setState({
+                    width: ref.style.width,
+                  });
+                  break;
+                case 'bottom':
+                  this.setState({
+                    height: ref.style.height,
+                  });
+                  break;
+                case 'left':
+                  this.setState({
+                    x: e.clientX,
+                    xUpdated: true,
+                    width: ref.style.width,
+                  });
+                  break;
+                case 'topLeft':
+                  this.setState({
+                    x: e.clientX,
+                    y: e.clientY,
+                    xUpdated: true,
+                    yUpdated: true,
+                    width: ref.style.width,
+                    height: ref.style.height,
+                  });
+                  break;
+                case 'topRight':
+                  this.setState({
+                    y: e.clientY,
+                    yUpdated: true,
+                    width: ref.style.width,
+                    height: ref.style.height,
+                  });
+                  break;
+                case 'bottomRight':
+                  this.setState({
+                    width: ref.style.width,
+                    height: ref.style.height,
+                  });
+                  break;
+                case 'bottomLeft':
+                  this.setState({
+                    x: e.clientX,
+                    xUpdated: true,
+                    width: ref.style.width,
+                    height: ref.style.height,
+                  });
+                  break;
+                default:
+                  break;
+              }
+            }}
             onResizeStop={(e, direction, ref, delta, position) => {
-              this.props.model.updateSize(parseFloat(ref.style.width), parseFloat(ref.style.height));
-              this.setState({
-                width: ref.style.width,
-                height: ref.style.height,
-                ...position
-              });
+              if (this.state.xUpdated && this.state.yUpdated === false) {
+                this.props.model.setPosition(parseFloat(this.state.x), this.props.model.getOption('position').y);
+              } else if (this.state.yUpdated && this.state.xUpdated === false) {
+                this.props.model.setPosition(this.props.model.getOption('position').x, parseFloat(this.state.y));
+              } else if (this.state.xUpdated && this.state.yUpdated) {
+                this.props.model.setPosition(parseFloat(this.state.x), parseFloat(this.state.y));
+              }
+              this.props.model.updateSize(this.state.width, this.state.height);
+              this.setState({x: 0, y: 0, xUpdated: false, yUpdated: false});
             }}
           >
-            <Chip 
-              icon={<img style={{cursor: 'pointer'}} 
-              src={MORE_OPTION} alt="" onClick={() => {this.props.openComposition(this.props.model)}} />} 
-              label={this.props.model.getOption('name')} 
-              color="secondary" 
+            <Chip
+              icon={<img style={{cursor: 'pointer'}}
+              src={MORE_OPTION} alt="" onClick={() => {this.props.openComposition(this.props.model)}} />}
+              label={this.props.model.getOption('name')}
+              color="secondary"
             />
           </Rnd>
         </Box>
