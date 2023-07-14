2d0070c176b1524edae714f442fc4608a1dec003
afonso <afonsobspinto@gmail.com>
2023-07-14 15:35:48 +0100

    PSYNEU-81 fix: Update composition to take model dimensions when not in resizing mode

--- src/client/components/views/editView/compositions/Composition.js
+++ src/client/components/views/editView/compositions/Composition.js
@@ -1,257 +1,282 @@
 import * as React from "react";
-import { Rnd } from "react-rnd";
-import { connect } from "react-redux";
-import { withStyles } from "@mui/styles";
-import { Box, Chip } from "@mui/material";
+import {Rnd} from "react-rnd";
+import {connect} from "react-redux";
+import {withStyles} from "@mui/styles";
+import {Box, Chip} from "@mui/material";
 import vars from "../../../../assets/styles/variables";
 import MORE_OPTION from "../../../../assets/svg/option.svg"
-import { openComposition } from "../../../../redux/actions/general";
+import {openComposition} from "../../../../redux/actions/general";
 import {RESIZE_CHANGED_POS_OPTION} from "../../../../../constants";
 import withParentListener from "../withParentListener";
 import withClipPath from "../withClipPath";
 import {CallbackTypes} from "@metacell/meta-diagram";
 
 const {
-  chipBorderColor,
-  chipTextColor,
-  draggableBg,
-  listItemActiveBg,
-  textWhite,
+    chipBorderColor,
+    chipTextColor,
+    draggableBg,
+    listItemActiveBg,
+    textWhite,
 } = vars;
 
 const commonStyles = {
-  background: `${textWhite} !important`,
-  border: `0.0975rem solid ${listItemActiveBg} !important`,
-  borderRadius: '0.125rem !important'
+    background: `${textWhite} !important`,
+    border: `0.0975rem solid ${listItemActiveBg} !important`,
+    borderRadius: '0.125rem !important'
 };
 
 const styles = () => ({
-  root: {
-    '& .react-draggable': {
-      background: draggableBg,
-      border: `0.125rem solid ${chipBorderColor}`,
-      borderRadius: '0.75rem',
-      display: "flex !important",
-      alignItems: "center",
-      justifyContent: "center",
+    root: {
+        '& .react-draggable': {
+            background: draggableBg,
+            border: `0.125rem solid ${chipBorderColor}`,
+            borderRadius: '0.75rem',
+            display: "flex !important",
+            alignItems: "center",
+            justifyContent: "center",
 
-      '&:hover': {
-        borderColor: listItemActiveBg
-      },
+            '&:hover': {
+                borderColor: listItemActiveBg
+            },
+        },
+
+        '& .MuiChip-root': {
+            background: chipBorderColor,
+            borderRadius: '0.75rem',
+            padding: '0 0.5rem',
+            display: "flex",
+            left: 0,
+            position: 'absolute',
+            color: chipTextColor,
+            top: '-1.75rem',
+            alignItems: "center",
+            height: '1.5rem',
+            letterSpacing: '-0.005rem',
+            fontWeight: 510,
+            fontSize: '0.8125rem',
+            lineHeight: '1.25rem',
+            flexDirection: 'row-reverse',
+
+            '& .MuiChip-label': {
+                padding: 0,
+            },
+
+            '& .MuiChip-icon': {
+                margin: '0 0 0 0.25rem',
+            },
+        },
     },
 
-    '& .MuiChip-root': {
-      background: chipBorderColor,
-      borderRadius: '0.75rem',
-      padding: '0 0.5rem',
-      display: "flex",
-      left: 0,
-      position: 'absolute',
-      color: chipTextColor,
-      top: '-1.75rem',
-      alignItems: "center",
-      height: '1.5rem',
-      letterSpacing: '-0.005rem',
-      fontWeight: 510,
-      fontSize: '0.8125rem',
-      lineHeight: '1.25rem',
-      flexDirection: 'row-reverse',
+    selected: {
+        '&:before': {
+            left: 0,
+            ...commonStyles
+        },
 
-      '& .MuiChip-label': {
-        padding: 0,
-      },
+        '&:after': {
+            right: 0,
+            ...commonStyles
+        },
 
-      '& .MuiChip-icon': {
-        margin: '0 0 0 0.25rem',
-      },
+        '& .MuiChip-root': {
+            background: listItemActiveBg
+        },
+
+        '& .react-draggable': {
+            borderColor: listItemActiveBg,
+        }
     },
-  },
-
-  selected: {
-    '&:before': {
-      left: 0,
-      ...commonStyles
-    },
-
-    '&:after': {
-      right: 0,
-      ...commonStyles
-    },
-
-    '& .MuiChip-root': {
-      background: listItemActiveBg
-    },
-
-    '& .react-draggable': {
-      borderColor: listItemActiveBg,
-    }
-  },
 });
 
 class Composition extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      expanded: false,
-      subGraphOpen: false,
-      width: props.model.options.width,
-      height: props.model.options.height,
-      x: 0,
-      y: 0,
-      xUpdated: false,
-      yUpdated: false,
-      isResizing: false
+    constructor(props) {
+        super(props);
+        this.state = {
+            expanded: false,
+            subGraphOpen: false,
+            width: props.model.width,
+            height: props.model.height,
+            x: 0,
+            y: 0,
+            xUpdated: false,
+            yUpdated: false,
+            isResizing: false
+        }
+        this.changeVisibility = this.changeVisibility.bind(this);
     }
-    this.changeVisibility = this.changeVisibility.bind(this);
-  }
 
-  componentDidMount() {
-    const {model, forceHOCUpdate} = this.props;
-    model.registerListener({
-      [CallbackTypes.POSITION_CHANGED]: (_) => {
-        forceHOCUpdate();
-      },
-    });
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    const { isResizing } = this.state;
-
-    // If we've started resizing, remove clipPath from parentElement
-    if (prevState.isResizing !== isResizing && isResizing) {
-      const parentElement = this.props.elementRef.current.parentElement;
-      parentElement.style.clipPath = '';
+    componentDidMount() {
+        const {model, forceHOCUpdate} = this.props;
+        model.registerListener({
+            [CallbackTypes.POSITION_CHANGED]: (_) => {
+                forceHOCUpdate();
+            },
+        });
     }
-    this.setZIndex()
-  }
 
-  setZIndex() {
-    const containerElement = this.props.elementRef.current.parentElement;
-    containerElement.style.zIndex = this.props.model.getGraphPath().length;
-  }
+    componentDidUpdate(prevProps, prevState) {
+        const {isResizing} = this.state;
 
+        if (this.hasResizeStarted(prevState, isResizing)) {
+            this.setClipPath('')
+        }
 
-  changeVisibility() {
-    this.setState({expanded: !this.state.expanded});
-  }
+        if (!isResizing && this.didModelDimensionsChange()) {
+            this.setState({
+                width: this.props.model.width,
+                height: this.props.model.height
+            });
+        }
 
-  render() {
-    const { expanded } = this.state;
-    const { classes, forceHOCUpdate, elementRef, engine, model, openComposition } = this.props;
+        this.updateZIndex()
+    }
 
-    return (
-        <Box
-          ref={elementRef}
-          style={{width: this.state.width, height: this.state.height}}
-          className={`${classes.root} ${expanded ? classes.selected : ''}`}
-        >
-          <Rnd
-            size={{ width: this.state.width, height: this.state.height }}
-            position={{ x: this.state.x, y: this.state.y }}
-            onResizeStart={(e, direction, ref, delta, position) => {
-              this.setState({ isResizing: true });
-            }}
-            onResize={(e, direction, ref, delta, position) => {
-              switch (direction) {
-                case 'top':
-                  this.setState({
-                    y: parseFloat(e.clientY),
-                    yUpdated: true,
-                    height: parseFloat(ref.style.height),
-                  });
-                  break;
-                case 'right':
-                  this.setState({
-                    width: parseFloat(ref.style.width),
-                  });
-                  break;
-                case 'bottom':
-                  this.setState({
-                    height: parseFloat(ref.style.height),
-                  });
-                  break;
-                case 'left':
-                  this.setState({
-                    x: parseFloat(e.clientX),
-                    xUpdated: true,
-                    width: parseFloat(ref.style.width),
-                  });
-                  break;
-                case 'topLeft':
-                  this.setState({
-                    x: parseFloat(e.clientX),
-                    y: parseFloat(e.clientY),
-                    xUpdated: true,
-                    yUpdated: true,
-                    width: parseFloat(ref.style.width),
-                    height: parseFloat(ref.style.height),
-                  });
-                  break;
-                case 'topRight':
-                  this.setState({
-                    y: parseFloat(e.clientY),
-                    yUpdated: true,
-                    width: parseFloat(ref.style.width),
-                    height: parseFloat(ref.style.height),
-                  });
-                  break;
-                case 'bottomRight':
-                  this.setState({
-                    width: parseFloat(ref.style.width),
-                    height: parseFloat(ref.style.height),
-                  });
-                  break;
-                case 'bottomLeft':
-                  this.setState({
-                    x: parseFloat(e.clientX),
-                    xUpdated: true,
-                    width: parseFloat(ref.style.width),
-                    height: parseFloat(ref.style.height),
-                  });
-                  break;
-                default:
-                  break;
-              }
-            }}
-            onResizeStop={(e, direction, ref, delta, position) => {
-              const chipHeight = Array.from(ref.childNodes).find((child) =>
-                  child.className.includes('MuiChip-root')).clientHeight * (engine.getModel().getZoomLevel() / 100) * 2;
-              if (this.state.xUpdated && this.state.yUpdated === false) {
-                model.setOption(RESIZE_CHANGED_POS_OPTION, true, false);
-                model.setPosition(parseFloat(this.state.x), model.getPosition().y)
-              } else if (this.state.yUpdated && this.state.xUpdated === false) {
-                model.setOption(RESIZE_CHANGED_POS_OPTION, true, false);
-                model.setPosition(model.getPosition().x, parseFloat(this.state.y) - chipHeight);
-              } else if (this.state.xUpdated && this.state.yUpdated) {
-                model.setOption(RESIZE_CHANGED_POS_OPTION, true, false);
-                model.setPosition(parseFloat(this.state.x), parseFloat(this.state.y) - chipHeight);
-              }
-              model.updateSize(this.state.width, this.state.height);
-              this.setState({x: 0, y: 0, xUpdated: false, yUpdated: false, isResizing: false});
-              forceHOCUpdate();
-            }}
-          >
-            <Chip
-              icon={<img style={{cursor: 'pointer'}}
-              src={MORE_OPTION} alt="" onClick={() => {openComposition(model)}} />}
-              label={model.getOption('name')}
-              color="secondary"
-            />
-          </Rnd>
-        </Box>
-    );
-  }
+    didModelDimensionsChange() {
+        return this.state.width !== this.props.model.width ||
+            this.state.height !== this.props.model.height;
+    }
+
+    hasResizeStarted(prevState, isResizing) {
+        return prevState.isResizing !== isResizing && isResizing;
+    }
+
+    setClipPath(clipPath) {
+        const containerElement = this.getContainerElement();
+        containerElement.style.clipPath = clipPath;
+    }
+
+    updateZIndex() {
+        const containerElement = this.props.elementRef.current.parentElement;
+        containerElement.style.zIndex = this.props.model.getGraphPath().length;
+    }
+
+    getContainerElement() {
+        return this.props.elementRef.current?.parentElement;
+    }
+
+    changeVisibility() {
+        this.setState({expanded: !this.state.expanded});
+    }
+
+    render() {
+        const {expanded} = this.state;
+        const {classes, forceHOCUpdate, elementRef, engine, model, openComposition} = this.props;
+
+        return (
+            <Box
+                ref={elementRef}
+                style={{width: this.state.width, height: this.state.height}}
+                className={`${classes.root} ${expanded ? classes.selected : ''}`}
+            >
+                <Rnd
+                    size={{width: this.state.width, height: this.state.height}}
+                    position={{x: this.state.x, y: this.state.y}}
+                    onResizeStart={(e, direction, ref, delta, position) => {
+                        this.setState({isResizing: true});
+                    }}
+                    onResize={(e, direction, ref, delta, position) => {
+                        switch (direction) {
+                            case 'top':
+                                this.setState({
+                                    y: parseFloat(e.clientY),
+                                    yUpdated: true,
+                                    height: parseFloat(ref.style.height),
+                                });
+                                break;
+                            case 'right':
+                                this.setState({
+                                    width: parseFloat(ref.style.width),
+                                });
+                                break;
+                            case 'bottom':
+                                this.setState({
+                                    height: parseFloat(ref.style.height),
+                                });
+                                break;
+                            case 'left':
+                                this.setState({
+                                    x: parseFloat(e.clientX),
+                                    xUpdated: true,
+                                    width: parseFloat(ref.style.width),
+                                });
+                                break;
+                            case 'topLeft':
+                                this.setState({
+                                    x: parseFloat(e.clientX),
+                                    y: parseFloat(e.clientY),
+                                    xUpdated: true,
+                                    yUpdated: true,
+                                    width: parseFloat(ref.style.width),
+                                    height: parseFloat(ref.style.height),
+                                });
+                                break;
+                            case 'topRight':
+                                this.setState({
+                                    y: parseFloat(e.clientY),
+                                    yUpdated: true,
+                                    width: parseFloat(ref.style.width),
+                                    height: parseFloat(ref.style.height),
+                                });
+                                break;
+                            case 'bottomRight':
+                                this.setState({
+                                    width: parseFloat(ref.style.width),
+                                    height: parseFloat(ref.style.height),
+                                });
+                                break;
+                            case 'bottomLeft':
+                                this.setState({
+                                    x: parseFloat(e.clientX),
+                                    xUpdated: true,
+                                    width: parseFloat(ref.style.width),
+                                    height: parseFloat(ref.style.height),
+                                });
+                                break;
+                            default:
+                                break;
+                        }
+                    }}
+                    onResizeStop={(e, direction, ref, delta, position) => {
+                        const chipHeight = Array.from(ref.childNodes).find((child) =>
+                            child.className.includes('MuiChip-root')).clientHeight * (engine.getModel().getZoomLevel() / 100) * 2;
+                        if (this.state.xUpdated && this.state.yUpdated === false) {
+                            model.setOption(RESIZE_CHANGED_POS_OPTION, true, false);
+                            model.setPosition(parseFloat(this.state.x), model.getPosition().y)
+                        } else if (this.state.yUpdated && this.state.xUpdated === false) {
+                            model.setOption(RESIZE_CHANGED_POS_OPTION, true, false);
+                            model.setPosition(model.getPosition().x, parseFloat(this.state.y) - chipHeight);
+                        } else if (this.state.xUpdated && this.state.yUpdated) {
+                            model.setOption(RESIZE_CHANGED_POS_OPTION, true, false);
+                            model.setPosition(parseFloat(this.state.x), parseFloat(this.state.y) - chipHeight);
+                        }
+                        model.updateSize(this.state.width, this.state.height);
+                        this.setState({x: 0, y: 0, xUpdated: false, yUpdated: false, isResizing: false});
+                        forceHOCUpdate();
+                    }}
+                >
+                    <Chip
+                        icon={<img style={{cursor: 'pointer'}}
+                                   src={MORE_OPTION} alt="" onClick={() => {
+                            openComposition(model)
+                        }}/>}
+                        label={model.getOption('name')}
+                        color="secondary"
+                    />
+                </Rnd>
+            </Box>
+        );
+    }
 }
 
-function mapDispatchToProps (dispatch) {
-  return {
-    openComposition: (node) => dispatch(openComposition(node)),
-  }
+function mapDispatchToProps(dispatch) {
+    return {
+        openComposition: (node) => dispatch(openComposition(node)),
+    }
 }
 
 export default connect(
     null,
     mapDispatchToProps,
     null,
-    { forwardRef: true },
+    {forwardRef: true},
 )(withParentListener(withStyles(styles)(withClipPath(Composition))));
