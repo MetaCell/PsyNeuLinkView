3a413d8ed57f229cc6ef56c6f295a9ec5cf4aefe
afonso <afonsobspinto@gmail.com>
2023-07-14 12:35:17 +0100

    PSYNEU-81 fix: Update graph updates to use visible bounding box

--- src/client/model/graph/MetaGraph.ts
+++ src/client/model/graph/MetaGraph.ts
@@ -3,6 +3,7 @@ import {MetaLink, MetaNodeModel, MetaLinkModel} from "@metacell/meta-diagram"
 import {Point} from "@projectstorm/geometry";
 import {MetaGraphEventTypes} from "./eventsHandler";
 import {arePathsDifferent, getNewPath} from "./utils";
+import {getClippingHelper} from "../clipping/ClippingHelperFactory";
 
 /**
  * Represents a tree node with a MetaNodeModel and its children Graph nodes.
@@ -369,11 +370,13 @@ export class MetaGraph {
      */
     getDeepestCompositionAtPoint(cursorX: number, cursorY: number, metaNodeModel: MetaNodeModel): MetaNodeModel | undefined {
         const parent = this.getParent(metaNodeModel);
-
-        if (parent && parent.getBoundingBox().containsPoint(new Point(cursorX, cursorY))) {
-            const parentGraph = this.getNodeGraph(parent.getGraphPath())
-            return this._getDeepestCompositionAtPointAux(cursorX, cursorY,
-                Array.from(parentGraph.getChildrenGraphs().values()), metaNodeModel) || parent;
+        if (parent) {
+            const parentClippingHelper = getClippingHelper(parent);
+            if (parentClippingHelper.getVisibleBoundingBox().containsPoint(new Point(cursorX, cursorY))) {
+                const parentGraph = this.getNodeGraph(parent.getGraphPath())
+                return this._getDeepestCompositionAtPointAux(cursorX, cursorY,
+                    Array.from(parentGraph.getChildrenGraphs().values()), metaNodeModel) || parent;
+            }
         } else {
             return this._getDeepestCompositionAtPointAux(cursorX, cursorY,
                 Array.from(this.roots.values()), metaNodeModel);
@@ -398,7 +401,8 @@ export class MetaGraph {
                 continue;
             }
 
-            if (node.getBoundingBox().containsPoint(new Point(cursorX, cursorY))) {
+            const nodeClippingHelper = getClippingHelper(node);
+            if (nodeClippingHelper.getVisibleBoundingBox().containsPoint(new Point(cursorX, cursorY))) {
                 // If the current node is the parent of the metaNodeModel, we'll only explore that branch
                 if (node.getID() === parentId) {
                     const deeperNode = this._getDeepestCompositionAtPointAux(cursorX, cursorY, Array.from(graph.getChildrenGraphs().values()), metaNodeModel);
