d3479f37c52544f4e1313a6e05ded9fb3a302be8
ddelpiano <dario@metacell.us>
2023-10-04 10:56:12 +0200

    poc for pip installation

--- pre_installation.sh
+++ pre_installation.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-PSNL_HOME=`pwd`
+#PSNL_HOME=`pwd`
 cd ../
-git clone https://github.com/PrincetonUniversity/PsyNeuLink
+#git clone https://github.com/PrincetonUniversity/PsyNeuLink
 cd PsyNeuLink
-git checkout 12bc1298fe
+#git checkout 12bc1298fe
 pip install -e .
 cd $PSNL_HOME
