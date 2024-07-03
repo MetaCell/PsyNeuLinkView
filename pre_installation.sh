60c3d5bf0f77657b85ad4a5de1caeab154a37697
Dario Del Piano <dario@metacell.us>
2024-07-04 01:42:52 +0200

    fixing conflicts

--- pre_installation.sh
+++ pre_installation.sh
@@ -4,10 +4,6 @@ PSNL_HOME=`pwd`
 cd ../
 git clone https://github.com/PrincetonUniversity/PsyNeuLink
 cd PsyNeuLink
-<<<<<<< release/0.0.5-fixes -- Incoming Change
 git checkout master
-=======
-git checkout devel
->>>>>>> develop -- Current Change
 pip install -e .
 cd $PSNL_HOME
