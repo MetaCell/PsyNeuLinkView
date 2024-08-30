f9fc165431104052202dc2878bbc5dd4db867d4e
jrmartin <jesus@metacell.us>
2024-08-30 18:10:16 +0200

    #PSYNEU-138 - Add installation scripts and update read me

---
+++ package/mac_installer.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash -l
+pip install -vv psyneulinkviewer --break-system-packages && source ~/.bash_profile && conda activate psyneulinkview && open /usr/local/bin/psyneulinkviewer-darwin-x64/psyneulinkviewer.app/
